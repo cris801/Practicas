328e60c6d7d826e1bb27d56ffab79f34ed4870ac
el | Cris <48634410+cris801@users.noreply.github.com>
2020-09-25 00:09:26 -0500

    revision general 1
    revision general fue concluida con exito

--- app/src/main/java/com/example/practicas/Activities/TweenActivity.java
+++ app/src/main/java/com/example/practicas/Activities/TweenActivity.java
@@ -2,6 +2,7 @@ package com.example.practicas.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.number.Scale;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -16,7 +17,15 @@ public class TweenActivity extends AppCompatActivity {
     // declaración global de las variables a utilizar a lo largo del código
     private ImageView ivBumeran;
     private Button btnRun;
+    private Button btnAlpha;
+    private Button btnRotate;
+    private Button btnScale;
+    private Button btnTranslate;
     private Animation animation;
+    private Animation animationAlpha;
+    private Animation animationRotate;
+    private Animation animationScale;
+    private Animation animationTranslate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +34,21 @@ public class TweenActivity extends AppCompatActivity {
 
         // conectando los componentes visuales con la parte logica de la app
         ivBumeran = (ImageView) findViewById(R.id.imageViewBlue);
+        btnRun = (Button) findViewById(R.id.buttonAnimations);
+        btnAlpha = (Button) findViewById(R.id.buttonAlpha);
+        btnRotate = (Button) findViewById(R.id.buttonRotate);
+        btnScale = (Button) findViewById(R.id.buttonScale);
+        btnTranslate = (Button) findViewById(R.id.buttonTranslate);
 
-        //
+        //conectamos el componente xml que contiene las intrucciones por medio de item's a los Animations
         animation = AnimationUtils.loadAnimation(TweenActivity.this, R.anim.animation);
+        animationAlpha = AnimationUtils.loadAnimation(TweenActivity.this, R.anim.alpha);
+        animationRotate = AnimationUtils.loadAnimation(TweenActivity.this, R.anim.rotate);
+        animationScale = AnimationUtils.loadAnimation(TweenActivity.this, R.anim.scale);
+        animationTranslate = AnimationUtils.loadAnimation(TweenActivity.this, R.anim.translate);
+
 
         // Asignando el setOnClickListener
-        btnRun = (Button) findViewById(R.id.buttonAnimations);
         btnRun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,7 +56,34 @@ public class TweenActivity extends AppCompatActivity {
                 ivBumeran.startAnimation(animation);
             }
         });
-
+        btnAlpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // iniciando la animación
+                ivBumeran.startAnimation(animationAlpha);
+            }
+        });
+        btnRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // iniciando la animación
+                ivBumeran.startAnimation(animationRotate);
+            }
+        });
+        btnScale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // iniciando la animación
+                ivBumeran.startAnimation(animationScale);
+            }
+        });
+        btnTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // iniciando la animación
+                ivBumeran.startAnimation(animationTranslate);
+            }
+        });
 
         // para el boton de back
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
